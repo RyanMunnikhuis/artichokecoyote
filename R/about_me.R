bf83f5e5b0fc40c8dd7d85c6ccb9846a891ba7ad
Ryanmunnikhuis <rmunnikhuis@bren.ucsb.edu>
2021-08-13 11:53:25 -0700

    Initial commit

---
+++ R/about_me.R
@@ -0,0 +1,14 @@
+#' Name and Date :)
+#'
+#' @param name insert your name here
+#' @param day insert a day of the week
+#' @param day_number insert the numerical day of the week (e.g., tuesday = 5)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+about_me = function(name, day, day_number) {
+  print(paste0("Hello, ", name, " is today ", day, " the ", day_number, "th?"))
+}
+
